00126cee4c3086e5bdab861ae55b20825707cd7e
JustABearOz <eddie_debear@hotmail.com>
2019-02-05 13:51:31 +1100

    Chaning Git Colors

--- Cool.psm1
+++ Cool.psm1
@@ -85,4 +85,5 @@ $sl.Colors.WithBackgroundColor = [ConsoleColor]::Blue
 $sl.Colors.VirtualEnvBackgroundColor = [System.ConsoleColor]::Red
 $sl.Colors.VirtualEnvForegroundColor = [System.ConsoleColor]::White
 $sl.Colors.CommandFailedIconForegroundColor = [System.ConsoleColor]::Red
+$sl.Colors.GitNoLocalChangesAndAheadColor = [System.ConsoleColor]::Blue
 ##$sl.PromptSymbols.ElevatedSymbol = '!'
